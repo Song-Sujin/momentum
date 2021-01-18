c0e6f74ddf6590ee83abdc63d35dd148b3465be2
Song-Sujin <ssj1744@naver.com>
2021-01-18 17:57:40 +0900

    1.10 완료

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-const a = 221;
-let b = a - 5;
-a = 4;
-console.log(b, a);
+const nicoInfo = {
+    name:"Nico",
+    age:33,
+    gender:"Male",
+    favMovies: ["Along the gods", "LOTR", "Oldboy"],
+    favFood: [
+        {
+            name:"Kimchi", 
+            fatty: false
+        }, 
+        {
+            name:"Cheese burger", 
+            fatty: true
+        }
+    ]   // favFood Object안에 Array가 있고 그 안에 Object가 있음
+}
+console.log(nicoInfo.gender);
+nicoInfo.gender = "Femail";     // const라도 객체 안에있는 정보들은 이렇게 변경 가능하다
+console.log(nicoInfo.gender);
+
+console.log(nicoInfo.favFood[0].fatty)  // false 출력
+
